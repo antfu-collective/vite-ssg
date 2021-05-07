32b9ca6c8fe9df4df4db5693d77bf21b0b8a317d
Joe Pantazidis <joe.pantazidis@outlook.de>
2021-05-07 16:14:19 +0200

    docs: add initial state to single-page example
    In order to demonstrate the usage in a single-page app

--- examples/single-page/src/main.ts
+++ examples/single-page/src/main.ts
@@ -1,4 +1,23 @@
+import { createPinia } from 'pinia'
 import { ViteSSG } from 'vite-ssg/single-page'
+import { useRootStore } from './store/root'
 import App from './App.vue'
 
-export const createApp = ViteSSG(App, null)
+export const createApp = ViteSSG(
+  App,
+  ({ app, initialState }) => {
+    const pinia = createPinia()
+    app.use(pinia)
+
+    if (import.meta.env.SSR) {
+      // this will be stringified and set to window.__INITIAL_STATE__
+      initialState.pinia = pinia.state.value
+    } else {
+      // on the client side, we restore the state
+      pinia.state.value = initialState.pinia || {}
+    }
+
+    const store = useRootStore(pinia)
+    store.initialize()
+  },
+)
---
+++ examples/single-page/src/store/root.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+
+interface RootState {
+  user: Record<string, any> | null
+}
+
+export const useRootStore = defineStore({
+  id: 'root',
+  state: (): RootState => ({
+    user: null,
+  }),
+  getters: {
+    isReady(state) {
+      return !!state.user
+    },
+  },
+  actions: {
+    initialize() {
+      if (this.isReady) return
+
+      console.log('Initialize user ...')
+      this.user = {
+        id: 1,
+        firstName: 'Jane',
+        lastName: 'Doe',
+      }
+    },
+  },
+})
