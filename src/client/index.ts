a919b3905757c4c3f3c828b3ca58ff82422cec2b
Joe Pantazidis <joe.pantazidis@outlook.de>
2021-07-10 10:13:54 +0200

    style: fix import order

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,9 +1,9 @@
 import { createSSRApp, Component, createApp as createClientApp } from 'vue'
 import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
 import { createHead, HeadClient } from '@vueuse/head'
-import type { RouterOptions, ViteSSGContext, ViteSSGClientOptions } from '../types'
 import { deserializeState, serializeState } from '../utils/state'
 import { ClientOnly } from './components/ClientOnly'
+import type { RouterOptions, ViteSSGContext, ViteSSGClientOptions } from '../types'
 
 export * from '../types'
 
