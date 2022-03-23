3adec80118d6f008fd2f6ffafcc08cb0101284ce
Johann Schopplich <mail@johannschopplich.com>
2022-03-23 13:51:32 +0100

    refactor: remove critters fs import workaround (#224)

--- src/node/critical.ts
+++ src/node/critical.ts
@@ -5,20 +5,7 @@ export async function getCritters(outDir: string, options: Options = {}): Promis
   try {
     const CrittersClass = (await import('critters')).default
 
-    // temporary workaround for: https://github.com/GoogleChromeLabs/critters/issues/94
-    const fs = await import('fs')
-
-    // Critters.readFile() somehow accepts this.fs and uses it instead of require('fs'). The latter throws an error, because require() is not available in ESM.
-    class CrittersClassWithFS extends CrittersClass {
-      fs: typeof fs
-
-      constructor(...args: ConstructorParameters<typeof CrittersClass>) {
-        super(...args)
-        this.fs = fs
-      }
-    }
-
-    return new CrittersClassWithFS({
+    return new CrittersClass({
       path: outDir,
       logLevel: 'warn',
       external: true,
