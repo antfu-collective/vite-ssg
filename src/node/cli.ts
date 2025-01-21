b8a1fcb39cf65139b35fa17eff0103eb80ba1b02
Reinaldo Mendes <725300+reinaldomendes@users.noreply.github.com>
2025-01-21 03:33:20 -0300

    separate generation of vite builds and includedRoutes generations into separated process. Saves memory

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,8 +1,13 @@
 import process from 'node:process'
-import { bold, gray, red, reset, underline } from 'kolorist'
+import { bold, gray, green, red, reset, underline } from 'kolorist'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import { build } from './build'
+import { promisify } from 'node:util'
+import { ChildProcess, spawn as doSpawn } from 'node:child_process'
+import { join } from 'node:path'
+
+const subProcessSet = new Set<ChildProcess>();
 
 // eslint-disable-next-line ts/no-unused-expressions
 yargs(hideBin(process.argv))
@@ -33,11 +38,27 @@ yargs(hideBin(process.argv))
       .option('skip-build', {
         type: 'boolean',
         describe: 'Skip build if already have build in ssg-out dir',
+      })
+      .option('build-only',{
+        type: 'boolean',
+        describe: "Build Server and client files, but not generate"
+      }).
+      option('ssgOut', {
+        type: 'string',
+        describe: 'SSR production build folder'
       }),
     async (args) => {
-      const { config: configFile = undefined, ...ssgOptions } = args
-
-      await build(ssgOptions, { configFile })
+      if (process.env.IS_CHILD) {
+        const { config: configFile = void 0, ...ssgOptions } = args;
+        await build(ssgOptions, { configFile });
+      } else {
+        const ssgOut = args.ssgOut ?? join(".vite-ssg-temp", Math.random().toString(36).substring(2, 12));      
+        
+        const _args = process.argv.slice(1);      
+        
+        await spawn(process.argv0, [..._args, '--build-only', '1'], { IS_CHILD: "1", SSG_OUT:ssgOut  });
+        await spawn(process.argv0, [..._args, '--skip-build', '1'], { IS_CHILD: "1", SSG_OUT:ssgOut  });
+      }
     },
   )
   .fail((msg, err, yargs) => {
@@ -50,3 +71,69 @@ yargs(hideBin(process.argv))
   .argv
 
 export {}
+
+
+
+
+/**
+ * @param  command
+ */
+export async function spawn(command: string, args: string[], env?: Record<string, string|undefined>) {  
+  const envStr = Object.entries(env || {}).reduce((acc,[key,value]) => {
+    return `${acc} ${key}='${value}' `
+  }, '');
+  console.info(`SPAWN: ${envStr} ${command} ${args.join(' ')}`)
+
+  type CallbackFunctionT = (err: null | string, result: { stdout: string, stderr: string }) => void
+  function fnDoSpawnWithCallback(command: string, cb: CallbackFunctionT) {
+    // console.log(command)
+
+    const subprocess = doSpawn(command, args, {
+      cwd: process.cwd(),
+      env: { ...env ?? {}, ...process.env },
+    })
+    subProcessSet.add(subprocess)
+    const encoding = 'utf-8'
+    let bufStdErr = Buffer.alloc(0, '', encoding)
+    let bufStdOut = Buffer.alloc(0, '', encoding)    
+    subprocess.stdout?.on('data', (data: Buffer) => {      
+      data.toString(encoding).split(/\n/).forEach((line) => {
+        line = `${line}\n`
+        if (line.startsWith('[')){
+          line = green(line)          
+        }
+
+        process.stderr.write(line)
+      })
+      bufStdOut = data      
+    })
+    subprocess.stderr?.on('data', (data: Buffer) => {
+      bufStdErr = data
+      data.toString(encoding).split(/\n/).forEach((line) => {
+        line = `${line}\n`
+        if (line.startsWith('[')) {
+          line = red(line)
+        }
+        process.stderr.write(line)
+      })
+    })   
+
+    subprocess.on('close', (code:any) => {
+      const stdout = `${bufStdOut.toString(encoding)}`
+      const stderr = `${bufStdErr.toString(encoding)}`
+      const result = { stderr, stdout }
+      subProcessSet.delete(subprocess)
+      if (code !== 0) {
+        if(code === null){
+          const errLine = red(new Error('Process killed').message);
+          process.stderr.write(errLine)        
+        }
+        return cb(`error code ${code} \n ${subprocess.stderr?.read()}`, result)
+      }
+      return cb(null, result)
+    })
+  }
+
+  return await promisify(fnDoSpawnWithCallback)(command)
+
+};
